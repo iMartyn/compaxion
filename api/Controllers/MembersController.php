b58a9837e9658ee880093c6d18899586aeaac628
Martyn Ranyard <ranyardm@gmail.com>
2014-01-23 22:23:05 +0000

    Add functionality for member check in/out

--- api/Controllers/MembersController.php
+++ api/Controllers/MembersController.php
@@ -13,11 +13,32 @@ class MembersController extends Controller {
         $this->mongoDbConnection = new MongoClient;
         $this->mongoDatabase = $this->mongoDbConnection->compaxion;
         $this->membersCollection = $this->mongoDatabase->members;
+        $this->listenerController = $di['ListenerController'];
+        //This line simply allows mqtt publishing without actually causing a hook.
+        $this->listenerController->listenEvent('member.status.changed',function (){},true);
     }
 
     public function checkAuthorisation(\Slim\Route $route) {
         //TODO: Actually verify auth
         return true;
+    }
+
+    public function checkMemberInOrOut($username,$in) {
+        $document = $this->membersCollection->findOne(array('username'=>$username),array('username'=>true,'checked_in'=>true));
+        if ($document['checked_in'] != $in) {
+            $this->membersCollection->update(array('username'=>$username),array('$set'=>array('checked_in'=>$in)));
+            $document['checked_in'] = $in;
+            $this->listenerController->triggerEvent('member.status.changed',array('checked_in' => $in,'username' => $username));
+        }
+        return $document;
+    }
+
+    public function checkinMemberByUsername($username) {
+        return $this->checkMemberInOrOut($username,true);
+    }
+
+    public function checkoutMemberByUsername($username) {
+        return $this->checkMemberInOrOut($username,false);
     }
 
     public function getMemberCount() {
